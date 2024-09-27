70c96cf6f736f5009cbcd73f6026180e8c6ad5f2
Guilherme Carvalho <guicarv99@gmail.com>
2024-09-27 17:52:04 +0100

    added the functionality to the albumcard

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+import CommentSection from "./CommentSection";
+import RatingComponent from "./RatingComponent";
+
 const AlbumCard = ({ album }) => {
+  const [comments, setComments] = useState(album.comments);
+  const [rating, setRating] = useState(album.rating || 0);
+
+  const handleAddComment = (newComment) => {
+    setComments([...comments, newComment]);
+  };
+
+  const handleRatingChange = (newRating) => {
+    setRating(newRating);
+  };
+
   return (
     <div className="album-card">
       <h3>{album.name}</h3>
       <p>Artist: {album.artist}</p>
       <p>Year: {album.year}</p>
       <p>Number of Songs: {album.songs.length}</p>
-      <p>Comments: {album.comments.length}</p>
+
+      <RatingComponent
+        currentRating={rating}
+        onRatingChange={handleRatingChange}
+      />
+
+      <CommentSection comments={comments} onAddComment={handleAddComment} />
     </div>
   );
 };
